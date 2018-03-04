a188b93559fd311d8dcabff887d8ddd5fed5a7aa
cpmarvin <cpmarvin@gmail.com>
2018-03-04 15:40:34 +0000

    Update getSPF.js

--- web_app/base/static/custom/topology/getSPF.js
+++ web_app/base/static/custom/topology/getSPF.js
@@ -2,7 +2,7 @@ function getSPF(source,target){
 	links_spf = []
 	links.forEach(function(d) { links_spf.push({source:d.source.name,target:d.target.name,metric:d.metric,id:d.link_id}) })
 	var arrStr = encodeURIComponent(JSON.stringify(links_spf));
-	url = "http://46.237.131.248:8801/api/spf?"+"arr="+arrStr+"&"+"source="+source+"&"+"target="+target
+	url = "http://X.X.X.X:8801/api/spf?"+"arr="+arrStr+"&"+"source="+source+"&"+"target="+target
 	var spf_results = $.ajax({type: "GET", url: url, async: false}).responseJSON;
 	return { spf_results}
 }
