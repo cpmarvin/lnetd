b4683eab68fda94a98e5cc5c3030f4d4f2fcd30d
cpmarvin <cpmarvin@gmail.com>
2019-04-06 19:39:32 +0000

    xr netconf support#2

---
+++ inputs/xr_rpc/mutils.py
@@ -0,0 +1,93 @@
+
+from lxml import etree as ETREE
+import pandas as pd
+from sqlalchemy import create_engine
+
+from lxml import etree, objectify
+from ncclient import manager
+from ncclient.xml_ import *
+
+sys.path.append('../utils/')
+from lnetd_log import get_module_logger
+
+logger = get_module_logger(__name__,'DEBUG')
+
+def remove_ns(xml_string):
+    '''Remove namespace from xml string'''
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.fromstring(xml_string, parser)
+    root = tree.getroottree()
+
+    for elem in root.getiterator():
+        if not hasattr(elem.tag, 'find'):
+            continue  # (1)
+        i = elem.tag.find('}')
+        if i >= 0:
+            elem.tag = elem.tag[i + 1:]
+    objectify.deannotate(root, cleanup_namespaces=True)
+    #return(etree.tostring(root, pretty_print=True))
+    logger.info('Remove namespace done')
+    return(root)
+
+
+def get_netconf(*args):
+    '''Get netconf operation data,
+    return clean xml object'''
+    results = []
+    ios_xr = manager.connect(host='10.13.13.13',
+                             username='cpetrescu',
+                             password='lab123',
+                             hostkey_verify=False,
+                             device_params={'name': 'iosxr'})
+    # just return static for now
+    for i in args:
+        logger.info('Get netconf data for %s' %i)
+        res = ios_xr.dispatch(to_ele(i))
+        res = to_xml(res.data)
+        results.append(remove_ns(res))
+    return results
+
+
+def parse_xml(db, result_tree):
+    if db == 'links':
+        isis_db = []
+        for isis_id in result_tree.iter('topology-brief'):
+            source = isis_id.xpath(".//igp-node-id")[0].text[:-3]
+            for link in isis_id.iter('topology-node-link'):
+                l_ip = link.xpath(".//topology-link-interface-address")[0].text
+                r_ip = link.xpath(".//topology-link-neighbor-address")[0].text
+                metric = link.xpath(".//topology-link-igp-metric")[0].text
+                target = link.xpath('topology-link-neighbor-system-id')[0].text[:-3]
+                # print target, l_ip, r_ip, metric
+                isis_dict = {'source': source, 'target': target, 'l_ip': l_ip, 'r_ip': r_ip, 'metric': metric}
+                isis_db.append(isis_dict)
+        df = pd.DataFrame(isis_db)
+        return df
+    if db == 'routers':
+        routers_db = []
+        for isis_id in result_tree.iter('topology-brief'):
+            name = isis_id.xpath(".//igp-node-id")[0].text[:-3]
+            ip = isis_id.xpath(".//topology-node-te-router-id")[0].text
+            routers_dict = {'name': name, 'ip': ip}
+            routers_db.append(routers_dict)
+        df = pd.DataFrame(routers_db)
+        return df
+    elif db == 'hostname':
+        hostname_db = {}
+        for hostname in result_tree.iter('host-names'):
+            for host_name in hostname:
+                name = host_name.xpath(".//host-name")[0].text
+                sys_id = host_name.xpath(".//system-id")[0].text
+                name_map = {sys_id: name}
+                hostname_db.update(name_map)
+        return hostname_db
+    else:
+        return('work in progress')
+
+def write_to_db(table,df):
+    try:
+        disk_engine = create_engine('sqlite:////opt/lnetd/web_app/database.db')
+        df.to_sql(table, disk_engine, if_exists='replace')
+    except Exception as e:
+        print(e)
+      
---
+++ inputs/xr_rpc/xr_rpc.py
@@ -0,0 +1,66 @@
+from mutils import get_netconf
+from mutils import parse_xml,write_to_db
+import pandas as pd
+
+import logging
+
+import sys
+sys.path.append('../utils/')
+from lnetd_log import get_module_logger
+
+logger = get_module_logger(__name__,'DEBUG')
+
+rpc_hostname = """
+<get xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <filter>
+    <isis xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-clns-isis-oper">
+      <instances>
+        <instance>
+          <host-names>
+            <host-name>
+              <host-name/>
+              <system-id/>
+            </host-name>
+          </host-names>
+          <instance-name/>
+        </instance>
+      </instances>
+    </isis>
+  </filter>
+</get>
+"""
+rpc_mpls = """
+<get xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <filter>
+    <mpls-te xmlns="http://cisco.com/ns/yang/Cisco-IOS-XR-mpls-te-oper">
+      <topology-briefs/>
+    </mpls-te>
+  </filter>
+</get>
+"""
+
+def main():
+    logger.info('Get Netconf data')    
+    mpls_te, isis_hostname = get_netconf(rpc_mpls,rpc_hostname)
+    logger.info('Parse xml data for hostname')
+    df_hostname = parse_xml('hostname', isis_hostname)
+    logger.info('Parse xml data for links')
+    df_links = parse_xml('links', mpls_te)
+    df_links['source'] = df_links.apply(lambda row: df_hostname[row['source']], axis=1)
+    df_links['target'] = df_links.apply(lambda row: df_hostname[row['target']], axis=1)
+    df_links.loc[:, 'l_ip_r_ip'] = pd.Series([tuple(sorted(each)) for each in list(zip(df_links.l_ip.values.tolist(), df_links.r_ip.values.tolist()))])
+    df_links['l_ip_r_ip'] = df_links['l_ip_r_ip'].astype(str)
+    print(df_links)
+    df_routers = parse_xml('routers', mpls_te)
+    df_routers['name'] = df_routers.apply(lambda row: df_hostname[row['name']], axis=1)
+    df_routers.loc[:, 'country'] = df_routers['name'].str[0:2]
+    #print(df_routers)
+    # write to db
+    logger.info('Write to Database')
+    write_to_db('rpc_routers',df_routers)
+    write_to_db('rpc_links',df_links)
+
+
+if __name__ == '__main__':
+    main()
+
