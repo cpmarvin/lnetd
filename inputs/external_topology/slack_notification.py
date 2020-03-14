3b99f10461f62e4de00212d07fe69449a41b787e
cpmarvin <cpmarvin@gmail.com>
2020-03-14 11:15:52 +0000

    remove dummy slack token

--- inputs/external_topology/slack_notification.py
+++ inputs/external_topology/slack_notification.py
@@ -34,7 +34,7 @@ def generate_alarm_text(rtr,interface):
 
 def send_slack_notification(rtr,interface):
     try:
-        client = slack.WebClient(token='xoxb-3738957452-1000578228822-xouk7yjHeKz2OEm16XjYRudb11111')
+        client = slack.WebClient(token='slack-token')
         #print(generate_alarm_text(rtr,interface))
         response = client.chat_postMessage(
             channel='UTF656E8K',
