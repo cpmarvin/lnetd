5b48a9218dff8fc153a08292fe05b1b5b79e8165
cpmarvin <cpmarvin@gmail.com>
2020-03-25 12:19:47 +0000

    remove legacy frontend

--- web_app/base/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
-
-## login and registration
-
-
-class LoginForm(FlaskForm):
-    username = TextField('Username')
-    password = PasswordField('Password')
--- web_app/objects/properties.py
+++
@@ -1,9 +0,0 @@
-from collections import OrderedDict
-
-pretty_names = OrderedDict([
-    ('juniper', 'Juniper'),
-    ('cisco-xr', 'Cisco'),
-    ('huawei', 'Huawei'),
-    ('cisco-ios', 'Cisco'),
-    ('NA', 'noSNMP'),
-    ])
